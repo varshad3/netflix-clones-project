956ee59dda44f96570dc75cee58dc51c9e4aae31
varshad3 <varshasuresh169@gmail.com>
2023-05-11 01:59:26 +0530

    netflix-clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from './components/Row';
+import requests from './request';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Netflix</h3>
+      <Row  title={'Netflix Originals'} fetchUrl={requests.fetchNetflixOriginals} />
     </div>
   );
 }
